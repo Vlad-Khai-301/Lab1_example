ebda43ba400ddadd4a4cb970406ad7a9df67720a
Vlad-Khai-301 <144373288+Vlad-Khai-301@users.noreply.github.com>
2023-09-22 09:47:22 +0000

    Оновлення коду

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
-import math # підключення бібліотеки
+import math  # підключення бібліотеки
+
 
 def task_integer1():
   """A distance L is given in centimeters.
   Find the amount of full meters of this distance
-  (1 m = 1000 cm)"""
-  try: # перевірка на помилки
-    l = int(input( "L (sm) = "))
-  except: # якщо помилка
-    print( "L must be an INTEGER !!!")
-    input( "Press enter for exit ...")
-  else: # якщо немає помилки
-    res = l // 100
-    print( "L = ", res, "m")
+  (1 m = 100 cm)"""
+  try:  # перевірка на помилки
+    length = int(input("L (sm) = "))
+  except ValueError:  # якщо помилка
+    print("L must be an INTEGER !!!")
+    input("Press enter for exit ...")
+  else:  # якщо немає помилки
+    res = length // 100
+    print("L = ", res, "m")
 
 
 def task2_0():
   """Calculate mathematical expression"""
-  print( " sin px - x ^ 3")
-  print( "y = _________________")
-  print( " ________________")
-  print( " V x + e ^ (x / 2)")
   try:
-    x = float(input("(Enter x> 0) x = "))
-  except:
+    x = float(input("\n(Enter x> 0) x = "))
+  except ValueError:
     print("L must be a NUMBER !!!")
-    input( "Press enter for exit ...")
+    input("Press enter for exit ...")
   else:
     try:
-      y = (math.sin(math.pi * x) - x ** 3) / \
-      math.sqrt(x + math.exp(0.5 * x))
-    except:
-      print( "x MUST be positive !!!")
-      input( "Press enter for exit ...")
+      denum = math.sin(math.pi * x) - x ** 3
+      num = math.sqrt(x + math.exp(0.5 * x))
+      y = denum / num
+    except ZeroDivisionError:
+      print("x MUST be positive !!!")
+      input("Press enter for exit ...")
     else:
-      print( "y = ", y)
+      print("y = ", y)
 
 
 def task_boolean1():
@@ -41,10 +39,25 @@ def task_boolean1():
   verify the following proposition:
   The number A is positive"""
   try:
-    A = int (input( "A = "))
-  except:
+    A = int(input("A = "))
+  except ValueError:
     print("A MUST be integer !!!")
     input("Press enter for exit ...")
   else:
-    res = A> 0
-    print( "A is positive: ", res)
+    res = A > 0
+    print("A is positive: ", res)
+
+
+def main():
+  """Основна функція для виклику одного із завдань за варіантом"""
+  task_num = int(input("Enter task num: "))
+  if task_num == 1:
+    task_integer1()
+  elif task_num == 2:
+    task2_0()
+  elif task_num == 3:
+    task_boolean1()
+  else:
+    print("Wrong task num")
+
+main()
